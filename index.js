1bfcdef200679896440c61fcb4ac6ae2ad260744
mnl187 <mnl187@gmail.com>
2022-08-16 22:52:30 +0200

    change index.js to class

--- index.js
+++ index.js
@@ -6,22 +6,39 @@ const {configuratorRouter} = require("./routes/configurator");
 const {orderRouter} = require("./routes/order");
 const {handlebarsHelpers} = require("./utils/handlebars-helpers");
 
-const app = express();
-
-app.use(express.json());
-app.use(express.static('public'));
-app.use(cookieParser());
-app.engine('.hbs', hbs.engine({
-    extname: '.hbs',
-    helpers: handlebarsHelpers,
-}));
-app.set('view engine', '.hbs');
-
-app.use('/', homeRouter);
-app.use('/configurator', configuratorRouter);
-app.use('/order', orderRouter);
 
 
-app.listen(3000, 'localhost', () => {
-    console.log('listening on http://localhost:3000');
-});
+class CookieMaker {
+constructor() {
+    this._configureApp();
+    this._setRoutes();
+    this._run();
+}
+
+_configureApp() {
+    this.app = express();
+
+    this.app.use(express.static('public'));
+    this.app.use(cookieParser());
+    this.app.use(express.json());
+    this.app.engine('.hbs', hbs.engine({
+        extname: '.hbs',
+        helpers: handlebarsHelpers,
+    }));
+    this.app.set('view engine', '.hbs');
+}
+
+_setRoutes() {
+    this.app.use('/', homeRouter);
+    this.app.use('/configurator', configuratorRouter);
+    this.app.use('/order', orderRouter);
+}
+
+_run() {
+    this.app.listen(3000, 'localhost', () => {
+        console.log('listening on http://localhost:3000');
+    });
+}
+}
+
+new App()
